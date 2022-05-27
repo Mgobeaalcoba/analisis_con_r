60fba9a5adb7484254bd4b9fe209c4f836edc68b
Marian Gobea <gobeamariano@gmail.com>
2022-05-27 13:31:02 -0300

    first pass with R 27.5.22

---
+++ Calculos.R
@@ -0,0 +1,26 @@
+4+2
+
+20-6
+
+25**2
+
+8^2
+
+View(orangeec)
+
+x <- 85
+
+x
+
+oficina = 8
+platzi=1
+viaje=1.5
+gimnasio=1
+
+tiempo_ocupado <- oficina + platzi + viaje + gimnasio
+tiempo_ocupado
+
+if (oficina==7) {platzi} else {viaje}
+
+help("mean")
+?median()
---
+++ mtcars describe.R
@@ -0,0 +1,2 @@
+str(mtcars) # Parecido a un describe de pandas. Me indica que tipos de datos tengo en mi dataset
+?mtcars # Me brinda info sobre mi dataset
